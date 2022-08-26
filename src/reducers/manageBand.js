ecd9f2db2eb9a2c00756023c8e522864775c439e
Sheena Bonilla <sheena.myb@gmail.com>
2022-08-26 13:52:06 -0700

    commit tree 9020aed3957eda6c5cdc3dd5968546acd43b46fa

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,29 @@
+import uuid from "uuid";
+
 export default function manageBand(state = {
   bands: [],
-}, action) {
+}, action) 
+{
+  console.log(action)
   switch(action.type) {
+    
     case "ADD_BAND":
-      return {...state, bands: state.bands.concat(action.name)}
+    
+      const band = {
+        id: uuid(),
+        name: action.name,
+      }
+      console.log( "state", {...state, bands: [...state.bands, band]})
+      return {...state, bands: [...state.bands, band]}
+    
+    case "DELETE_BAND":
+      debugger
+      const bands = state.bands.filter((band) => band.id !== action.id);
+
+      return { bands };
+
     default:
-      return state
+      return state;
   
   }
-};
+}
